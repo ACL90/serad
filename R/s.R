fe0676f1e97ae03a382a65b0c02e25841f1eefc0
ACL90 <alexandre.cazenave-lacroutz@travail.gouv.fr>
2023-04-13 11:51:08 +0200

    s : seuil à 2 et documentation
    Mise à jour fonction s
    Mise à jour des vignettes avec document()

--- R/s.R
+++ R/s.R
@@ -2,17 +2,24 @@
 #' @param a Le nombre
 #' @param sing Le mot à indiquer au singulier, rien par défaut.
 #' @param plur Le mot à indiquer au pluriel, -s par défaut (d'ou le nom de la fonction)
-#' @param seuil Pour gérer le cas limite où un arrondi à 2 depuis l'inférieur.
+#' @param seuil Pour gérer un arrondi à 2 depuis l'inférieur.
 #'
-#' @return Le mot bien accordé ; par défault '-s ou rien.
+#' @return Le mot bien accordé ; par défault '-s ou rien. TEST
 #'
 #' @examples
-#' s(-7.5)
-#' s(-2)
-#' s(1.4,"chat parle", "chats parlent")
-#' s(-2,"chat parle", "chats parlent")
+#' s(-7.5)             #s
+#' s(-2)               #s
+#' s(1.4,"chat parle", "chats parlent") #chat parle
+#' s(-2,"chat parle", "chats parlent")  #chats parlent
+#' s(1.97)             #NA
+#' s(round(1.97,1))    #s
+#' s(1.97, seuil=1.95) #s
+#'
+#' @seealso
+#' Le [site de l'Académie française](https://www.dictionnaire-academie.fr/article/QDL057)
 #'
 #' @export
-s=function(a,sing="",plur="s",seuil=1.95){
+s=function(a,sing="",plur="s",seuil=2){
   ifelse(abs(a)>=seuil,plur,sing)
 }
+
--- lancement_InterimMensuel.R
+++ lancement_InterimMensuel.R
@@ -147,12 +147,15 @@ render("InterimMensuel.Rmd",
 
 
 ################ Pendant le developpement du package #####################
+#il faudra décommenter et tester avec TRUE
 devtools::check(cran = FALSE)
 #https://stackoverflow.com/questions/38312576/package-vignettes-not-available-in-r
 #usethis::use_vignette("serad")
 #https://community.rstudio.com/t/bypass-qpdf-checks/115691
 #devtools::build_vignettes()  #https://stackoverflow.com/questions/38312576/package-vignettes-not-available-in-r
+#rmarkdown::render("vignettes/serad.Rmd")
 devtools::build()
+devtools::document()
 
 #devtools::load_all()
 #Le suivant nécessite de faire skip en ligne de commande
