3b3327c56ea778aa13b968afb99d275968241328
ACL90 <alexandre.cazenave-lacroutz@travail.gouv.fr>
2023-04-30 20:21:20 +0200

    ajustement documentation

--- R/plushautniveau.R
+++ R/plushautniveau.R
@@ -33,15 +33,12 @@
 #' col5 = c( 12,     11,      7,         3,     9,     4)
 #' df1 = data.frame(col0,col1,col2,col3,col4,col5)
 #' plushautniveau(df1) #"C'est le plus haut niveau depuis Y1T2."
-#' plushautniveau(df1,nbperiode = 5.000000000000001)  #erreur: nbperiode doit
-#' etre un nombre entier
+#' #plushautniveau(df1,nbperiode = 5.000000000000001)  #erreur: nbperiode doit etre un nombre entier
 #' plushautniveau(df1,nbperiode = 5)  #NA
-#' plushautniveau(df1,vary="col2") #"C'est le plus haut niveau depuis le
-#' début de la série."
-#' plushautniveau(df1,vary="col3") #"C'est le plus bas niveau depuis
-#' Y1trim3."
-#' plushautniveau(df1,vary="col4") #"C'est le plus bas niveau depuis le début de
-#'  la série."
+#' plushautniveau(df1,vary="col2") #"C'est le plus haut niveau depuis le début de la série."
+#' plushautniveau(df1,vary="col3") #"C'est le plus bas niveau depuis Y1trim3."
+#' plushautniveau(df1,vary="col4") #"C'est le plus bas niveau depuis le début de la série."
+#'
 #' @export
 plushautniveau = function(df,
                           temps = "croissant",
